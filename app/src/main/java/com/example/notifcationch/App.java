f108b1df8ba4aae7579c04e296340928de3fca53
sunshinejen <jennifer.villacis@seattlecentral.edu>
2019-06-04 23:17:43 -0700

    Updates to color and Action button

--- app/src/main/java/com/example/notifcationch/App.java
+++ app/src/main/java/com/example/notifcationch/App.java
@@ -16,23 +16,25 @@ public class App extends Application {
     @Override
     public void onCreate(){
         super.onCreate();
+
+        createNotificationChannels();
     }
 
     public void createNotificationChannels() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
-                    "channel1",
+                    "Channel 1",
                     NotificationManager.IMPORTANCE_HIGH
             );
-            channel1.setDescription("This is Channel1");
+            channel1.setDescription("This is Channel 1");
 
             NotificationChannel channel2 = new NotificationChannel(
                     CHANNEL_2_ID,
-                    "channel2",
+                    "Channel 2",
                     NotificationManager.IMPORTANCE_LOW
             );
-            channel1.setDescription("This is Channel2");
+            channel2.setDescription("This is Channel 2");
 
             NotificationManager manager = getSystemService(NotificationManager.class);
                 manager.createNotificationChannel(channel1);
